28e5414ee5ffc56b791c044cc4fddc8d0e8b1024
Tom Goren <tom.goren@globality.com>
2017-06-15 19:48:56 -0400

    Remove unnecessary optimization, update docker version, add sudo flag to reduce vague errors

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -18,11 +18,11 @@ echo "Installing pip..."
 sudo easy_install pip
 
 echo "Upgrading setuptools and pip..."
-sudo pip install -U setuptools
-sudo pip install -U pip
+sudo -H pip install -U setuptools
+sudo -H pip install -U pip
 
 echo "Installing Ansible..."
-sudo pip install ansible
+sudo -H pip install ansible
 
 echo "Installing git..."
 sudo apt-get install -y git
